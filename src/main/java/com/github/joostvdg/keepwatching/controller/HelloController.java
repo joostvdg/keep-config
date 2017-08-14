f71a4f7e04d6a784b2e565181080bc1e10ca09d7
Joost van der Griendt <joostvdg@gmail.com>
2017-08-14 22:21:59 +0200

    Add hello controller for testing

---
+++ src/main/java/com/github/joostvdg/keepwatching/controller/HelloController.java
@@ -0,0 +1,37 @@
+package com.github.joostvdg.keepwatching.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+
+    private static class Dummy {
+        private final String name;
+        private final boolean dummy;
+
+        private Dummy(String name, boolean dummy){
+            this.name = name;
+            this.dummy = dummy;
+        }
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public ResponseEntity<Collection<Dummy>> getTools(){
+        Dummy dummy = new Dummy("Dummy", true);
+        List<Dummy> dummies = new ArrayList<>();
+        dummies.add(dummy);
+        return ResponseEntity.ok().body(dummies);
+    }
+}
