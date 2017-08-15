33b59cd2e04812a82eabf9e70c6bc2731fafdd50
Joost van der Griendt <joostvdg@gmail.com>
2017-08-15 22:22:51 +0200

    Add minimal cloud config configuration

--- src/main/java/com/github/joostvdg/keepwatching/RestServiceApplication.java
+++ src/main/java/com/github/joostvdg/keepwatching/RestServiceApplication.java
@@ -2,8 +2,10 @@ package com.github.joostvdg.keepwatching;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class RestServiceApplication {
 
     public static void main(String[] args) {
